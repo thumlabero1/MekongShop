137f4ea12570dabaeab9e71627a94fc1f452e301
Phan_Linh <nhomtoanhsg20172018@gmail.com>
2024-08-21 20:10:48 +0700

    started

---
+++ my-nextjs-app/public/schema.js
@@ -0,0 +1,16 @@
+<script type='application/ld+json'>
+            
+                context = 'https://schema.org',
+                type=  "WebSite",
+                name= 'Ninico - Minimal eCommerce',
+                url= 'https://ninico.botble.com',
+            
+            </script>
+
+
+context =  "https:\/\/schema.org",
+type = "Organization",
+name = "Ninico - Minimal eCommerce",
+url = "https:\/\/ninico.botble.com",
+logo = type= "ImageObject",
+url = "https:\/\/ninico.botble.com\/storage\/general\/logo.png"
---
+++ my-nextjs-app/src/app/page.tsx
@@ -0,0 +1,144 @@
+import Image from "next/image";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import '../styles/fonts.css';
+import '../styles/variables.css';
+export default function Home() {
+  return (
+    <><html>
+          <head>
+
+              <link rel="dns-prefetch" href="//ninico.botble.com"> </link>
+              <link rel="dns-prefetch" href="//schema.org"></link>
+              <link rel="dns-prefetch" href="//www.w3.org"></link>
+              <link rel="dns-prefetch" href="//www.facebook.com"></link>
+              <link rel="dns-prefetch" href="//twitter.com"></link>
+              <link rel="dns-prefetch" href="//www.behance.net"></link>
+              <link rel="dns-prefetch" href="//www.youtube.com"></link>
+              <link rel="dns-prefetch" href="//www.linkedin.com"></link>
+              <link rel="dns-prefetch" href="//www.googletagmanager.com"></link>
+
+              <Helmet>
+                  <meta charSet="utf-8" />
+                  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                  <meta
+                      name="viewport"
+                      content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1" />
+                  <meta
+                      name="csrf-token"
+                      content="op0Fj5s8M6yXuDdZPLkaczDqXtZp6ePuq8LFYOMM" />
+              </Helmet>
+
+              const GlobalStyle = createGlobalStyle`
+
+
+              `;
+
+
+
+              <title>Ninico - Minimal eCommerce</title>
+              <meta name='description' content="Ninico is a modern and flexible Minimal eCommerce Laravel script. This script is suited for electronic, organic and grocery store, furniture store, clothing store, hitech store and accessories store..." />
+              <link rel="canonical" href="https://ninico.botble.com"></link>
+              <meta name='robots' content="index, follow" />
+              <meta property="og:site_name" content="Ninico - Minimal eCommerce" />
+              <meta property="og:type" content="article" />
+              <meta property="og:title" content="Ninico - Minimal eCommerce" />
+              <meta property="og:description" content="Ninico is a modern and flexible Minimal eCommerce Laravel script. This script is suited for electronic, organic and grocery store, furniture store, clothing store, hitech store and accessories store..." />
+              <meta property="og:url" content="https://ninico.botble.com" />
+              <meta name='twitter:title' content="Ninico - Minimal eCommerce" />
+              <meta name='twitter:description' content="Ninico is a modern and flexible Minimal eCommerce Laravel script. This script is suited for electronic, organic and grocery store, furniture store, clothing store, hitech store and accessories store..." />
+
+
+              {/* </head>/* /*image-x-icon </meta></meta><link rel="shortcut icon" type=image/x-icon href="https://ninico.botble.com/storage/general/favicon.png"> */}
+
+
+
+
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/vendor/core/plugins/language/css/language-public.css?v=2.2.0"> </link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/vendor/core/plugins/cookie-consent/css/cookie-consent.css?v=1.0.2"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/vendor/core/plugins/sale-popup/css/sale-popup.css?v=1.0.0"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/vendor/core/plugins/newsletter/css/newsletter.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/vendor/core/core/base/libraries/ckeditor/content-styles.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/bootstrap/bootstrap.min.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/animate/animate.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/swiper/swiper-bundle.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/slick/slick.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/nice-select/nice-select.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/css/fontawesome.min.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/magnific-popup/magnific-popup.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/plugins/jquery-ui/jquery-ui.css"></link>
+              <link media="all" type='text/css' rel="stylesheet" href="https://ninico.botble.com/themes/ninico/css/theme.css?v=1.6.0"></link>
+              
+          <link media="all" type = 'text' />css rel="stylesheet" href="https://ninico.botble.com/vendor/core/plugins/announcement/css/announcement.css"> </link>
+          <script>
+              window.siteUrl = "https://ninico.botble.com";
+          </script>
+          <style>
+              .page_speed_1772106784 {background}: url('https://ninico.botble.com/storage/general/newsletter-popup.png') no-repeat;
+              background-position: center;
+              background-size: cover;
+              }
+
+              .page_speed_20334912 {display}: none
+              }
+
+              .page_speed_1643747642 {--background - color}: #f3eee7;
+              --text-color: #000000;
+              --font-size: 13px;
+              }
+
+              .page_speed_672215905 {justify - content}: start;
+              max-width: 1200px;
+              }
+
+              .page_speed_516788916 {margin - inline - end}: 0.5rem}
+
+              .page_speed_1285431249 {display}: none;
+              }
+
+              .page_speed_2096156492 {height}: 16px
+              }
+
+              .page_speed_1116810447 {max - height}: 35px
+              }
+
+              .page_speed_1492887563
+
+              .page_speed_1760053804 {height}: 20px
+              }
+
+              .page_speed_670431883 {background - color}: #F8F8F8;
+              --footer-text-color: #000000;
+              --footer-text-muted-color: #686666;
+              --footer-border-color: #E0E0E0;
+              }
+
+              .page_speed_504988643 {background - color}: #ededed
+              }
+
+              .page_speed_1421990164 {inset - inline - end}: 2px
+              }
+
+              .page_speed_1810463683 {background - color}: #000;
+              color: #fff;
+              }
+
+              .page_speed_1896222129 {max - width}: 1170px;
+              }
+
+              .page_speed_1773715850 {background - color}: #000;
+              color: #fff;
+              border: 1px solid #fff;
+              }
+          </style>
+      </head><body>
+              <div>
+                  <h1>Hello, Next.js!</h1>
+                  <p>This is a simple HTML page rendered using server-side rendering (SSR).</p>
+              </div>
+          </body></>
+      </html>
+   
+
+  );
+}
